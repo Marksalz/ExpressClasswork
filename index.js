665471163a5077e23c6a949ae121fc315609692f
Mark <menachemsalzberg13@gmail.com>
2025-07-08 17:18:49 +0300

    fix: improve response format and logging in /greet endpoints; add action handling. Added /action endpoint using cat api to test using outside apis with an api_key

--- index.js
+++ index.js
@@ -5,34 +5,57 @@ const server = express();
 
 function logger(req, res, next) {
     req.startTime = new Date().toLocaleString();
-    console.log(`request with ${req.method} method, url: ${req.url}\nstartTime: ${req.startTime}\n`);
+    console.log(`request with ${req.method} method, url: ${req.url}, startTime: ${req.startTime}\n`);
     next();
 }
 
 server.use(express.json(), logger);
 
 server.get('/greet', (req, res) => {
-    res.send(JSON.stringify({ msg: `hi from get endpoint ${new Date().toLocaleString()}` }));
+    res.send({ msg: `hi from get endpoint ${new Date().toLocaleString()}` });
 });
 
 server.get('/greet/:name', (req, res) => {
     console.log(`I got name: ${req.params.name}\n`);
-    res.send(JSON.stringify({ msg: `got name: ${req.params.name}` }));
+    res.send({ msg: `got name: ${req.params.name}` });
 });
 
 server.get('/test', async (req, res) => {
-    const response = await fetch('http://localHost:4545/greet/Bob',);
+    const response = await fetch('http://localhost:4545/greet/Bob');
     const data = await response.json();
-    console.log(data);
     if ("msg" in data && data.msg === "got name: Bob") {
-        res.send(JSON.stringify({ result: "ok" }));
+        return res.send({ result: "ok" });
     }
     else {
-        res.send(JSON.stringify({ result: "fail" }));
-        
+        return res.send({ result: "fail" });
     }
 });
 
+server.post('/action', async (req, res) => {
+    if (!req.body.action || (req.body.action !== "joke" && req.body.action !== "cat fact")) {
+        return res.status(400).send({ msg: "body is malformed" });
+    }
+    else {
+        if (req.body.action === "joke") {
+            const response = await fetch("https://official-joke-api.appspot.com/random_joke");
+            const data = await response.json();
+            return res.send({ joke: `${data.setup.toUpperCase()} ${data.punchline.toUpperCase()}` });
+        } else if (req.body.action === "cat fact") {
+            const api_key = "live_WghUt5yfrePa7WhYrHpclGEKqANg5NFVH50ls15M9UZZ7mUmdmDg6KPNmVUv51o2";
+            const response = await fetch(
+                "https://api.thecatapi.com/v1/images/search?limit=12",
+                {
+                    headers: {
+                        "x-api-key": api_key
+                    }
+                }
+            );
+            const data = await response.json();
+            return res.send({ length: `${data.length}` });
+        }
+    }
+})
+
 server.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}\n`);
-})
+});
