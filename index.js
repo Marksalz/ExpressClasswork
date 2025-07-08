b095fcbdccbf1d7c42f467fdbaee377868588840
Mark <menachemsalzberg13@gmail.com>
2025-07-08 15:31:54 +0300

    feat: add logger middleware and update response format for /greet endpoints

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ const PORT = 4545;
 
 const server = express();
 
-server.use(express.json());
+function logger(req, res, next) {
+    req.startTime = new Date().toLocaleString();
+    console.log(`request with ${req.method} method, url: ${req.url}\nstartTime: ${req.startTime}\n`);
+    next();
+}
+
+server.use(express.json(), logger);
 
 server.get('/greet', (req, res) => {
-    res.send({ "msg": `hi from get endpoint ${new Date().toLocaleString()}` });
+    res.send(JSON.stringify({ msg: `hi from get endpoint ${new Date().toLocaleString()}` }));
 });
 
 server.get('/greet/:name', (req, res) => {
-    console.log(`I got name: ${req.params.name}`);
-    res.send({ "msg": `got name: ${req.params.name}` });
+    console.log(`I got name: ${req.params.name}\n`);
+    res.send(JSON.stringify({ msg: `got name: ${req.params.name}` }));
+});
+
+server.get('/test', async (req, res) => {
+    const response = await fetch('http://localHost:4545/greet/Bob',);
+    const data = await response.json();
+    console.log(data);
+    if ("msg" in data && data.msg === "got name: Bob") {
+        res.send(JSON.stringify({ result: "ok" }));
+    }
+    else {
+        res.send(JSON.stringify({ result: "fail" }));
+    }
 });
 
 server.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+    console.log(`Listening on port: ${PORT}\n`);
 })
